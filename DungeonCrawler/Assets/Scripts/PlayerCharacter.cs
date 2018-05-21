980b801350ead351f62af5387acd3665443a91d4
Tagflag2d <39307114+Tagflag2d@users.noreply.github.com>
2018-05-21 17:57:15 +0200

    Improved Camera Rotation and Follow

--- DungeonCrawler/Assets/Scripts/PlayerCharacter.cs
+++ DungeonCrawler/Assets/Scripts/PlayerCharacter.cs
@@ -8,12 +8,15 @@ public class PlayerCharacter : MoveableObject
     private Vector3 ownPos; // the coord to the point where the camera looks at
 
     float oldAngle;
+    public Vector3 cameraOffset;
+    public bool cameraFollow = true;
 
     // Use this for initialization
     void Start()
     {
         ownPos = transform.position; // get target's coords
         Camera.main.transform.LookAt(ownPos); // makes the camera look to it 
+        cameraOffset = Camera.main.transform.position;
     }
 
     // Update is called once per frame
@@ -42,28 +45,55 @@ public class PlayerCharacter : MoveableObject
             if (Input.GetKeyDown(KeyCode.Q))
             {
                 movingFinished = false;
+                cameraFollow = false;
                 StartCoroutine("SmoothRotationPlayer", -45);
             }
             else if (Input.GetKeyDown(KeyCode.W))
             {
                 movingFinished = false;
+                cameraFollow = false;
                 StartCoroutine("SmoothRotationPlayer", 45);
             }
+
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                Camera.main.transform.RotateAround(transform.position, Vector3.up, 45);
+            }
         }
+
+    }
+
+    private void LateUpdate()
+    {
+        // Camera follow player
+        if (cameraFollow)
+            Camera.main.transform.position = transform.position+cameraOffset;
     }
 
     IEnumerator SmoothRotationPlayer(float angle)
     {
-        Quaternion fromAngle = transform.rotation;
-        Quaternion toAngle = Quaternion.Euler(transform.eulerAngles + Vector3.up * angle);
+        Vector3 startPosition = Camera.main.transform.position;
+        Quaternion startRotation = Camera.main.transform.rotation;
 
-        for (float t = 0f; t < 1; t += Time.deltaTime / rotationTime)
+        Camera.main.transform.RotateAround(transform.position, Vector3.up, angle);
+        Vector3 targetPosition = Camera.main.transform.position;
+        Quaternion targetRotation = Camera.main.transform.rotation;
+
+        Camera.main.transform.position = startPosition;
+        Camera.main.transform.rotation = startRotation;
+
+        for (float t = 0f; t < 0.9; t += Time.deltaTime / rotationTime)
         {
-            transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+            Camera.main.transform.RotateAround(transform.position, Vector3.up, angle * Time.deltaTime / rotationTime);
             yield return null;
         }
 
-        transform.rotation = toAngle;
+        Camera.main.transform.position = targetPosition;
+        Camera.main.transform.rotation = targetRotation;
+
+        cameraOffset = targetPosition - transform.position;
+        cameraFollow = true;
+
         movingFinished = true;
     }
 }
